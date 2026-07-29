447c66ba1266098745f3e8fcb868179000fcad9c
Nurul Yakim Kazal <kazal.yakim@gmail.com>
2026-07-29 21:02:34 +0800

    Update scheduler to include companies ETL

--- modules/init_pipeline.py
+++ modules/init_pipeline.py
@@ -1,25 +1,27 @@
-from src.db.stock_database import fetch_stock_data
 from src.db.company_database import fetch_company_data
+from src.db.stock_database import fetch_stock_data
+
 
 _db_initialized = False
 
 
-def init_db_and_sync(create_tables, run_stock_etl, run_company_etl):
+def init_db_and_sync(create_companies_table, create_stock_table, run_company_etl, run_stock_etl):
     global _db_initialized
 
     # Create tables once
     if not _db_initialized:
-        create_tables()
+        create_companies_table()
+        create_stock_table()
         _db_initialized = True
 
     try:
-        run_stock_etl()
-        fetch_stock_data.cache_clear()
-
         run_company_etl()
         fetch_company_data.cache_clear()
 
-        print("Stock and company data sync completed.")
+        run_stock_etl()
+        fetch_stock_data.cache_clear()
+
+        print("Company and stock data sync completed.")
 
     except Exception as e:
         print(f"ETL failed: {e}")
--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -7,21 +7,23 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.append(str(PROJECT_ROOT))
 
 from modules.init_pipeline import init_db_and_sync
+from src.db.company_database import create_companies_table
 from src.db.stock_database import create_stock_table
-from src.etl.stock_incremental_etl import run_stock_etl
 from src.etl.company_etl import run_company_etl
+from src.etl.stock_incremental_etl import run_stock_etl
 
 
 def sync():
-    print(f"Starting stock sync: {datetime.now(timezone.utc)}")
+    print(f"Starting company and stock sync: {datetime.now(timezone.utc)}")
 
     init_db_and_sync(
+        create_companies_table,
         create_stock_table,
-        run_stock_etl,
-        run_company_etl
+        run_company_etl,
+        run_stock_etl
     )
 
-    print("Data sync complete")
+    print("Company and stock data sync complete")
 
 
 if __name__ == "__main__":
